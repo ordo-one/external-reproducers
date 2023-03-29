03ad0f60ad26f1090b07d7d0724823dcca92e755
Joakim Hassila <jocke@ordo.one>
2023-03-29 18:24:09 +0200

    Added benchmark from swift forums small fixes

--- benchmarks/elide-some/Package.swift
+++ benchmarks/elide-some/Package.swift
@@ -7,23 +7,11 @@ let package = Package(
     name: "elide-some",
     platforms: [.macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "elide-some",
-            targets: ["elide-some"]),
     ],
     dependencies: [
-        //        .package(url: "https://github.com/ordo-one/package-benchmark", branch: "main"),
          .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "1.4.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "elide-some"),
-        .testTarget(
-            name: "elide-someTests",
-            dependencies: ["elide-some"]),
     ]
 )
 
