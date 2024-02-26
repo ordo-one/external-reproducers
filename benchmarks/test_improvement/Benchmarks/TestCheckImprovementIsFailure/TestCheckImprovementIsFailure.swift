295f3e65bbceae419f0f8504d3c2b602e2b6c157
BlindSpot <127803250+blindspotbounty@users.noreply.github.com>
2024-02-26 11:16:00 +0300

    remove most of metrics

--- benchmarks/test_improvement/Benchmarks/TestCheckImprovementIsFailure/TestCheckImprovementIsFailure.swift
+++ benchmarks/test_improvement/Benchmarks/TestCheckImprovementIsFailure/TestCheckImprovementIsFailure.swift
@@ -36,24 +36,12 @@ extension Benchmark {
 
 let benchmarks = {
     Benchmark.defaultConfiguration = .init(
-        metrics: [.wallClock, .cpuTotal, .allocatedResidentMemory, .contextSwitches, .throughput] + .arc,
+        metrics: [.wallClock],
         warmupIterations: 0,
         scalingFactor: .one,
         maxDuration: .seconds(5),
-        maxIterations: 100/*,
-        thresholds: [
-            // Thresholds are wild guess mostly. Have to adjust with time.
-            .wallClock: .init(relative: [.p90: 10]),
-            .cpuTotal: .init(relative: [.p90: 0]),
-            .allocatedResidentMemory: .init(relative: [.p90: 20]),
-            .contextSwitches: .init(relative: [.p90: 10]),
-            .throughput: .init(relative: [.p90: 10]),
-            .objectAllocCount: .init(relative: [.p90: 10]),
-            .retainCount: .init(relative: [.p90: 10]),
-            .releaseCount: .init(relative: [.p90: 10]),
-            .retainReleaseDelta: .init(relative: [.p90: 10]),
-        ]*/
-    )
+        maxIterations: 100 
+   )
 
     Benchmark.setup = {
     }
