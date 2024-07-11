0ffd7f0e46c6f5c73a622558b7834ff4184d5d40
ser-0xff <122270051+ser-0xff@users.noreply.github.com>
2024-07-11 14:01:26 +0300

    Build key path.

--- swift/predicate-expr/Sources/main.swift
+++ swift/predicate-expr/Sources/main.swift
@@ -2,19 +2,40 @@ import Foundation
 
 protocol PredicateExpressionProviding {
     static func makeExpression() -> any PredicateExpression<Self>
+    static func buildKeyPath(_ rootOutputTypeName: String) -> any PredicateExpression<Self>
+    static func buildKeyPathWithRoot<T>(_ root: some PredicateExpression<T>) -> any PredicateExpression<Self>
 }
 
-extension String: PredicateExpressionProviding {
+struct MyStruct {
+    let value: Int = 10
+}
+
+extension MyStruct: PredicateExpressionProviding {
     static func makeExpression() -> any PredicateExpression<Self> {
-        return PredicateExpressions.build_Arg("arg")
+        PredicateExpressions.build_Arg(MyStruct())
     }
 }
 
-func makeExpression<CompareType>(_ compareType: CompareType.Type) -> any PredicateExpression<Bool>? {
-    if let compareType = compareType as? any PredicateExpressionProviding.Type {
-        let lhs = compareType.makeExpression()
-        let rhs = compareType.makeExpression()
-        return PredicateExpressions.build_Equal(lhs: lhs, rhs: rhs)
+extension Int: PredicateExpressionProviding {
+    static func makeExpression() -> any PredicateExpression<Self> {
+        fatalError()
     }
-    return nil
 }
+
+extension PredicateExpressionProviding {
+    static func buildKeyPath(_ rootOutputTypeName: String) -> any PredicateExpression<Self> {
+        guard let rootOutputType = _typeByName(rootOutputTypeName) else { fatalError() }
+        guard let rootOutputType = rootOutputType as? any PredicateExpressionProviding.Type else { fatalError() }
+        let root = rootOutputType.makeExpression()
+        return buildKeyPathWithRoot(root)
+    }
+
+    static func buildKeyPathWithRoot<T>(_ root: some PredicateExpression<T>) -> any PredicateExpression<Self> {
+        let keyPath = \MyStruct.value as! KeyPath<T, Self>
+        return PredicateExpressions.build_KeyPath(root: root, keyPath: keyPath)
+    }
+}
+
+guard let rootOutputTypeName = _mangledTypeName(String.self) else { fatalError() }
+let expr = Int.buildKeyPath(rootOutputTypeName)
+print("\(type(of: expr))")
