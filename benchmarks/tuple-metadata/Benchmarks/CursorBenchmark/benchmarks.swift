5d26c54dfb1a543fa7c71749331f5f8e0e769f35
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2025-04-04 11:57:44 +0300

    add task group and payload

--- benchmarks/tuple-metadata/Benchmarks/CursorBenchmark/benchmarks.swift
+++ benchmarks/tuple-metadata/Benchmarks/CursorBenchmark/benchmarks.swift
@@ -1,36 +1,58 @@
 import Benchmark
 import Cursor
 
-class A
+struct A
 {
-    init() {}
+    var data: [UInt8]
+    init() {
+        data = Array(repeating: 0, count: 1024)
+    }
 }
-class B
+struct B
 {
-    init() {}
+    var data: [UInt8]
+    init() {
+        data = Array(repeating: 0, count: 1024)
+    }
 }
 
 let benchmarks:@Sendable () -> () =
 {
     Benchmark.init("Tuple",
         configuration: .init(metrics: BenchmarkMetric.default,
-        maxDuration: .seconds(10)))
-    {
-        let ca:Cursor1<Int, A> = .init(key: 1, value: .init(), count: $0.scaledIterations.count)
-        for (key, value):(Int, A) in ca
-        {
-            blackHole((key, value))
+                             scalingFactor: .mega,
+                             maxDuration: .seconds(10)))
+    { benchmark in
+        let operations = benchmark.scaledIterations.count
+        await withTaskGroup(of: Void.self) { group in
+            for _ in 0 ..< 12 {
+                group.addTask {
+                    let ca:Cursor1<Int, A> = .init(key: 1, value: .init(), count: operations)
+                    for (key, value):(Int, A) in ca
+                    {
+                        blackHole((key, value))
+                    }
+                }
+            }
         }
     }
 
     Benchmark.init("Pair",
         configuration: .init(metrics: BenchmarkMetric.default,
-        maxDuration: .seconds(10)))
-    {
-        let cb:Cursor2<Int, B> = .init(key: 1, value: .init(), count: $0.scaledIterations.count)
-        for try next:Cursor2<Int, B>.Pair in cb
-        {
-            blackHole(next)
+                             scalingFactor: .mega,
+                             maxDuration: .seconds(10)))
+    { benchmark in
+        let operations = benchmark.scaledIterations.count
+        await withTaskGroup(of: Void.self) { group in
+            for _ in 0 ..< 12 {
+                group.addTask {
+                    let cb:Cursor2<Int, B> = .init(key: 1, value: .init(), count: operations)
+                    for next:Cursor2<Int, B>.Pair in cb
+                    {
+                        blackHole(next)
+                    }
+                }
+            }
         }
     }
 }
