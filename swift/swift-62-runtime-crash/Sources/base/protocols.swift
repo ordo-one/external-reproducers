dbf6688f045ac931a851a64ced9a9d8241eaed28
BlindSpot <127803250+blindspotbounty@users.noreply.github.com>
2025-10-03 17:23:27 +0200

    [sc-22398] reproduce swift rt crash

---
+++ swift/swift-62-runtime-crash/Sources/base/protocols.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public protocol Marker1: Sendable {}
+public protocol Marker2: Sendable {}
+
+public protocol Base<Object, ObjectEntry>: Sendable {
+    associatedtype Object: Marker1 & Marker2 & Identifiable
+    associatedtype ObjectEntry: Identifiable<Object.ID>
+
+    var predicate: Predicate<Self.Object> { get }
+
+    func entries() throws -> any Sequence<Object>
+}
+
+public protocol Proto1<Object>: Base where Object == ObjectEntry {
+}
+
+public struct TestIdentifiable: Identifiable, Marker2, Marker1, Sendable {
+    public let id: String
+
+    init(id: String) {
+        self.id = id
+    }
+}
