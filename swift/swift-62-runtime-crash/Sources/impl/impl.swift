207941952189f4b28a2f88a3175c525b5638dc94
blindspotbounty <127803250+blindspotbounty@users.noreply.github.com>
2025-10-03 17:39:06 +0200

    Merge pull request #40 from ordo-one/repro-rt-crash
    [sc-22398] reproduce swift rt crash

---
+++ swift/swift-62-runtime-crash/Sources/impl/impl.swift
@@ -0,0 +1,32 @@
+import base
+import Foundation
+import Synchronization
+
+private class CacheInstanceBase<Object: Identifiable>: @unchecked Sendable {
+    let predicate: Predicate<Object>?
+
+    init(predicate: Predicate<Object>) {
+        self.predicate = predicate
+    }
+}
+
+private final class Impl<Object: Marker1 & Marker2 & Identifiable & Sendable>: Proto1 where Object.ID: Sendable & Hashable {
+    typealias ObjectEntry = Object
+    private struct State: Sendable {
+        var predicate: Predicate<Object>?
+        var instance: CacheInstanceBase<Object>?
+        var array: [Object.ID: Object] = [:]
+    }
+
+    private let state: Mutex<State>
+
+    init(predicate: Predicate<Object>) {
+        state = .init(.init(predicate: predicate, instance: .init(predicate: predicate)))
+    }
+
+    var predicate: Predicate<Object> { state.withLock { $0.instance?.predicate ?? $0.predicate ?? .true } }
+}
+
+public func make<Object: Marker1 & Marker2 & Identifiable & Sendable>(predicate: Predicate<Object>) -> any Proto1<Object> where Object.ID: Sendable & Hashable {
+    return Impl<Object>(predicate: predicate)
+}
---
+++ swift/swift-62-runtime-crash/Sources/swift-62-runtime-crash/main.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Synchronization
+import base
+import impl
+
+
+let predicate = #Predicate<TestIdentifiable> {
+    $0.id == "test"
+}
+
+let cache = make(predicate: predicate)
+
+print(cache.predicate)
+
